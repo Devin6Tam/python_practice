ae320c94deda3e06e16feb011789f2d4a2cef6b6
tanxw <txtxw@163.com>
2020-04-09 22:44:43 +0800

    练习示例

--- lesson102/nine_nine_table.py
+++ lesson102/nine_nine_table.py
@@ -25,7 +25,18 @@ for i in range(1, 10):
     print(row_data)
 
 
-for i in range(1, 10):  # 1,2,3,4,5,6,7,8,9
-    for j in range(1, i+1):
-        print("%d*%d=%d" % (j, i, i*j), end="\t")
+# for i in range(1, 10):  # 1,2,3,4,5,6,7,8,9
+#     for j in range(1, i+1):
+#         print("%d*%d=%d" % (j, i, i*j), end="\t")
+#     print("")
+
+i = 1
+# 外循环
+while i < 10:
+    j = 1
+    # 内循环（1~i）
+    while j <= i:
+        print("%d*%d=%d" % (j, i, (i * j)), end='\t')
+        j += 1
     print("")
+    i += 1
